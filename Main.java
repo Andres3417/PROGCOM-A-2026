8cb578ee2d356db000b59c3425c0d08ed7eb3456
Andres3417 <arodriguez501@unab.edu.co>
2026-02-24 15:55:08 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,42 @@
+/******************************************************************************
+temp = float(input("temperatura registrada: "))
+
+if temp >= 27:
+  print("pongase algo fresco")
+elif temp >= 20 and temp < 27:
+  print("abrígate") 
+elif temp >= 16 and temp < 20:
+  print("abrigate más")
+else:
+  print("está helando")
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    System.out.println("Temperatura registrada: ");
+		Scanner leer = new Scanner (System.in);
+		
+		float temp = leer.nextFloat();
+		
+		if (temp >= 27){System.out.println("Póngase algo fresco");}
+		else if (temp>=20 && temp<27){System.out.println("abrigate");}
+		else if (temp>=16 && temp<20){System.out.println("abrigate más");}
+		else{System.out.println("está helado");}
+		
+		int edad = leer.nextInt():
+		System.out.println(ead>=18?"Eres mayor de edad"):"No eres mayor de edad")
+                
+                
+		System.out.println("cual es tu edad");
+		int edad = leer.nextInt();
+		System.out.println(ead>=18?"Eres mayor de edad"):"No eres mayor de edad");
+
+	    
+	
+		System.out.println("cual es tu edad");
+		int edad = leer.nextInt();
+		System.out.println(ead>=18?"Eres mayor de edad"):"No eres mayor de edad");
+		}
+	}
+        
